669ba987de7b16e65f02a192442a93044f695564
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-01-07 23:00:55 +0700

    install lottie and fix error

--- App.js
+++ App.js
@@ -4,17 +4,7 @@ import {View, Text, StyleSheet, Button, Alert} from 'react-native';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-
-<<<<<<< HEAD
-import HomeScreen from './src/components/HomeScreen';
-import DetailsScreen from './src/components/DetailsScreen';
-import RegistrationScreen from './src/components/RegistrationScreen';
-import DataDetailsScreen from './src/components/DataDetailsScreen';
-import ModalScreen from './src/components/ModalScreen';
-import FlatListScreen from './src/components/FlatListScreen';
-import NewsScreen from './src/components/NewsScreen';
-import TestReactNativePaper from './src/components/TestReactNativePaper';
-=======
+import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import RegistrationScreen from './src/screens/RegistrationScreen';
@@ -22,15 +12,14 @@ import DataDetailsScreen from './src/screens/DataDetailsScreen';
 import ModalScreen from './src/screens/ModalScreen';
 import FlatListScreen from './src/screens/FlatListScreen';
 import NewsScreen from './src/screens/NewsScreen';
-import SplashScreen from './src/screens/SplashScreen'
->>>>>>> 20d3de0ec6aadef22dd2f77aed5166cdaf6255cb
+import TestReactNativePaper from './src/components/TestReactNativePaper';
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       {
-        <Stack.Navigator initialRouteName="Splash">
+        <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Top Tab Example" component={DetailsScreen} />
